9f12163616c9e200e63b613ba424de2ff59e60f4
Rakesh <rakeshkraki3272@gmail.con>
2023-04-29 14:37:09 +0530

    complete login page

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,84 +1,162 @@
+
+
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _rememberMe = false;
+
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      Navigator.pushReplacementNamed(context, '/Newspage.drt');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/logos.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Scaffold(
-          backgroundColor:Colors.transparent,
-          body : Stack(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 80,top: 100),
-                child: Text(
-                  'Welcome Back',
-                style: TextStyle(color: Colors.black,fontSize: 35),
-                ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/logos.png"),
+                fit: BoxFit.cover,
               ),
-              SingleChildScrollView(
-                child: Container(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height*0.4,
-                    left: 35,
-                    right: 35,
-                  ),
+            ),
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
                   child: Column(
-                    children: [
-                      TextField(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(left: 0, top: 80),
+                        child: Text(
+                          'Welcome Back',
+                          style: TextStyle(color: Colors.black, fontSize: 35),
+                        ),
+                      ),
+                      SizedBox(height: 100),
+                      TextFormField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
                         decoration: InputDecoration(
-                          fillColor: Colors.grey.shade100,
-                          filled: true,
-                          hintText: 'Email',
+                          labelText: 'Email',
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.email),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                              .hasMatch(value)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          icon: Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          if (!RegExp(
+                              r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$')
+                              .hasMatch(value)) {
+                            return 'Please enter a valid password (minimum 8 characters with at least 1 letter and 1 number)';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 5),
+                      Row(
+                        children: <Widget>[
+                          Checkbox(
+                            value: _rememberMe,
+                            onChanged: (value) {
+                              setState(() {
+                                _rememberMe = value!;
+                              });
+                            },
+                          ),
+                          Text('Remember me                               forget password'),
+                        ],
+                      ),
+                      SizedBox(height: 20.0,),
+                      InkWell(
+                        onTap: _login,
+                        child: Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.indigo,
+                              borderRadius: BorderRadius.circular(5)
+                          ),
+                          child: Center(
+                            child: Text(
+                              'login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                      SizedBox(height: 20,),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                        fillColor: Colors.grey.shade100,
-                        filled: true,
-                        hintText: 'Password',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)
-                        ),
-                      ),),
-
-                      Container(
-                        padding: EdgeInsets.only(left: 200),
-                        child: Text('forget password',
-                        style: TextStyle(fontSize: 15),
-                        ),
-                      ),
-
-                      SizedBox(height: 16.0),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text('Login'),
-                      ),
-                      Container(
-                        child: Text('--------------------------or--------------------------',
-                          style: TextStyle(fontSize: 15),
-                        ),
+                      SizedBox(height: 0000000000000),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Need an account?',
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                          TextButton(onPressed: (){},child: Text(
+                            'Sign up',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ),
-              )
-            ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 }
+
